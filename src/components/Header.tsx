33626d62b22485ee2290cff4b3093d16935fcc7d
HectorAlrz <healcaraz@gmail.com>
2024-10-25 11:08:04 -0600

    feat: add react-icons and update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,15 @@ import SearchInput from "./SearchInput";
 
 function Header() {
   return (
-    <div>
-      <Link to="/">NPM Registry</Link>
-      <SearchInput />
+    <div className="flex items-center justify-between px-4 border-b h-14 mb-8">
+      <div className="w-full flex items-center space-x-2">
+        <Link to="/" className="text-lg font-bold">
+          NPM Registry
+        </Link>
+      </div>
+      <div className="w-full max-x-xl ml-4">
+        <SearchInput />
+      </div>
     </div>
   );
 }
